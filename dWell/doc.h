70bb48befd55240f43b1ba1cc6ee79f7f8547998
kashapovd <y.kashapovd@gmail.com>
2021-01-04 10:01:58 +0700

    Revert "added doxygen comments for doc class"
    This reverts commit 0495bf27b669b2cce656b5a7646050af411f6261.

--- dWell/doc.h
+++ dWell/doc.h
@@ -5,19 +5,10 @@
 
 #include <QPrinter>
 
-/**
- * @brief Класс справки.
- */
 class doc
 {
 public:
-    /// Конструктор по умолчанию
     doc() {}
-    /**
-     * @brief Статический метод, выполняющий сохранение PDF-файла справки.
-     * @param h Константный указатель на проживающего, для которого генерируется справка.
-     * @param printer Содержит настройки печати PDF-файла.
-     */
     static void generate(const habitant *h, QPrinter *printer);
 };
 
