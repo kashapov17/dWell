22649ec259043d3e0e0b476a3f9bda26b9eae7c8
kashapovd <y.kashapovd@gmail.com>
2021-01-04 20:43:32 +0700

    added doxygen comments for user class

--- dWell/user.h
+++ dWell/user.h
@@ -1,3 +1,10 @@
+/**
+ * @file user.h
+ * @brief Заголовчный файл класса user.
+ * @author Кашапов Ярослав
+ * @date 2021
+ */
+
 #ifndef USER_H
 #define USER_H
 
@@ -5,20 +12,40 @@
 
 #include <QString>
 
+/**
+ * @brief Класс комнаты.
+ * Наследуется от класса bookEntry.
+ */
 class user : public bookEntry
 {
 public:
+    /// Перечислитель, содержащий возможные типы пользователей.
     enum utype {ADMIN, COMMANDANT, STUDENT, UNKNOWN};
+    /// Конструктор по умолчанию.
     user() {}
+    /**
+     * @brief Дополнительный конструктор. Устанавливает
+     * соответствующие поля объекта класса user.
+     * @param name Имя пользователя
+     * @param pass Пароль пользователя.
+     * @param type Тип пользователя.
+     */
     user(QString name, QString pass, utype type);
+    /// Возвращает имя пользователя.
     const QString name() const;
+    /// Возвращает пароль пользователя.
     const QString passwd() const;
+    /// Возвращает тип пользователя.
     utype type() const;
-    bool setData(QString &, QString &, utype &);
+    /// Устанавливает поля объекта пользователя.
+    bool setData(QString &name, QString &pass, utype &type);
 
 private:
+    /// Поле имени пользователя.
     QString mName;
+    /// Поле пароля пользователя.
     QString mPasswd;
+    /// Поле типа пользователя.
     utype mType;
 
 protected:
