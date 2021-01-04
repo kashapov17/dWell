ac52e3270bedbd962becc19d0c16d8732e63a11f
kashapovd <y.kashapovd@gmail.com>
2021-01-04 16:50:25 +0700

    added doxygen comments for habitant class

--- dWell/habitant.h
+++ dWell/habitant.h
@@ -1,3 +1,10 @@
+/**
+ * @file habitant.h
+ * @brief Заголовчный файл класса habitant.
+ * @author Кашапов Ярослав
+ * @date 2021
+ */
+
 #ifndef HABITANT_H
 #define HABITANT_H
 
@@ -5,10 +12,16 @@
 #include <QDate>
 #include <QDataStream>
 
+/**
+ * @brief Класс проживающего.
+ */
 class habitant
 {
-
 public:
+    /**
+     * @brief Cтруктура, хранящая информацию о проживающем.
+     * Используется для перемещения этой информации внутри программы.
+     */
     struct habitantData
     {
         QString fname;
@@ -21,30 +34,54 @@ public:
         uint roomNumber;
     };
 
+    /// Конструктор класса.
     habitant() {}
+    /**
+     * @brief Дополнительный конструктор.
+     * @param hd Указатель на структуру данных.
+     */
     habitant(habitantData *hd);
+    /// Устанавливает поля данных, равным полям в структуре @p.
     bool setData(habitantData *hd);
+    /// Возвращает указатель на данные проживающего.
     habitantData *getData() const;
 
+    /// Возвращает имя проживающего.
     const QString fname() const;
+    /// Возвращает фамилию проживающего.
     const QString lname() const;
+    /// Возвращает отчество проживающего.
     const QString patronymic() const;
+    /// Возвращает дату рождения проживающего.
     const QDate birthDate() const;
+    /// Возвращает номер студенческого билета проживающего.
     uint studentID() const;
+    /// Возвращает номер курса обучения проживающего.
     uint numOfCourse() const;
+    /// Возвращает наименование группы, в которой обучается проживащий.
     QString group() const;
+    /// Возвращает номер комнаты проживающего.
     uint roomNumber() const;
 
 private:
+    /// Поле имени.
     QString mFname;
+    /// Поле фамилии.
     QString mLname;
+    /// Поле отчества.
     QString mPatronymic;
+    /// Поле даты рождения.
     QDate mBirthDate;
+    /// Поле номера студенческого билета.
     uint mStudentID;
+    /// Поле номера курса обучения.
     uint mNumOfCourse;
+    /// Поле номера группы обучения.
     QString mGroup;
+    /// Поле номера комнаты проживания.
     uint mRoomNumber;
 };
+
 
 inline QDataStream &operator<< (QDataStream &ost, const habitant &h)
 {
