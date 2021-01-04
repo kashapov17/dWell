581cc91e487a3397ac74a07c325caa2456478982
kashapovd <y.kashapovd@gmail.com>
2021-01-04 10:03:26 +0700

    added doxygen comments for doc class

--- dWell/doc.h
+++ dWell/doc.h
@@ -1,3 +1,10 @@
+/**
+ * @file doc.h
+ * @brief Заголовчный файл класса doc.
+ * @author Кашапов Ярослав
+ * @date 2021
+ */
+
 #ifndef DOC_H
 #define DOC_H
 
@@ -5,10 +12,19 @@
 
 #include <QPrinter>
 
+/**
+ * @brief Класс справки.
+ */
 class doc
 {
 public:
+    /// Конструктор по умолчанию
     doc() {}
+    /**
+     * @brief Статический метод, выполняющий сохранение PDF-файла справки.
+     * @param h Константный указатель на проживающего, для которого генерируется справка.
+     * @param printer Содержит настройки печати PDF-файла.
+     */
     static void generate(const habitant *h, QPrinter *printer);
 };
 
