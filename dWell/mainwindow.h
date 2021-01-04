e159f0769329ea92a77f81a5504eb7606b604673
kashapovd <y.kashapovd@gmail.com>
2021-01-04 17:04:44 +0700

    updated doxygen comments for MainWindow class

--- dWell/mainwindow.h
+++ dWell/mainwindow.h
@@ -1,14 +1,22 @@
+/**
+ * @file mainwindow.h
+ * @brief Заголовчный файл класса MainWindow.
+ * @author Кашапов Ярослав
+ * @date 2021
+ */
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
 
-#include "ubook.h"
-
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/**
+ * @brief Класс диалога входа в систему.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -21,9 +29,13 @@ private:
     Ui::MainWindow *ui;
 
 private slots:
+    /// Обрабатывает нажатие на кнопку "О Qt" в меню.
     void about_qt();
+    /// Обрабатывает нажатие на кнопку "О программе" в меню.
     void about_dwell();
+    /// Обрабатывает нажатие на кнопку "Об авторах" в меню.
     void about_autors();
+    /// Обрабатывает нажатие на кнопку "Войти".
     void on_loginButton_clicked();
 };
 #endif // MAINWINDOW_H
